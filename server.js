4b71897cedbe3c526783fb5eda1f1ddf2125f66b
Maxmengt <mengt2012@gmail.com>
2016-10-24 21:12:55 +0800

    add router module

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // 请求（ require） Node.js 自带的 http 模块，并且把它赋值给 http 变量
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
 	http.createServer(function(request, response) {
-		console.log("Request received.");
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+
+		route(pathname);
+
 		response.writeHead(200, {"Content-Type" : "text/plain"});
 		response.write("Hello World!");
 		response.end();
