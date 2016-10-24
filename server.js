a2df967ae7eb8c31a4ce47cf30217919b3d7aecb
Maxmengt <mengt2012@gmail.com>
2016-10-24 20:43:39 +0800

    update server.js and add new file index.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 // 请求（ require） Node.js 自带的 http 模块，并且把它赋值给 http 变量
 var http = require("http");
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type" : "text/plain"});
-	response.write("Hello World!");
-	response.end();
-}).listen(8888);
+function start() {
+	http.createServer(function(request, response) {
+		console.log("Request received.");
+		response.writeHead(200, {"Content-Type" : "text/plain"});
+		response.write("Hello World!");
+		response.end();
+	}).listen(8888);
+
+	console.log("Server has started.");
+}
+
+exports.start = start;
