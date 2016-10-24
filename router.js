ab60ff2cdbe3a93402d34e44f76fd85b3c2c1b98
Maxmengt <mengt2012@gmail.com>
2016-10-24 22:25:55 +0800

    add requestHandler module

--- router.js
+++ router.js
@@ -1,5 +1,13 @@
-function route(pathname) {
+var requestHandlers = require("./requestHandlers");
+
+function route(handle, pathname) {
 	console.log("About to route a request for " + pathname);
+	if( typeof handle[pathname] === 'function' ) {
+		handle[pathname]();
+	}
+	else {
+		console.log("No request handler found for " + pathname);
+	}
 }
 
 exports.route = route;
