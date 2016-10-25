4d9bf08ab42871d4c479ab57066a7a7da64c37fd
Maxmengt <mengt2012@gmail.com>
2016-10-25 14:11:21 +0800

    no block in code

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,23 @@
-function start() {
-	console.log("Request handler 'start' was called.");
-	function sleep(milliSeconds) {
-		var startTime = new Date().getTime();
-		while( new Date().getTime() < startTime + milliSeconds );
-	}
+var exec = require("child_process").exec;
 
-	sleep(5000);
-	return "Hello Start";
+function start(response) {
+	console.log("Request handler 'start' was called.");
+
+	// exec("ls -lah", 
+	exec("find /", { timeout: 10000, maxBuffer: 20000*1024 },
+		function(error, stdout, stderr) {
+			response.writeHead(200, {"Content-Type": "text/plain"});
+			response.write(stdout);
+			response.end();
+		}
+	);
 }
 
-function upload() {
+function upload(response) {
 	console.log("Request handler 'upload' was called.");
-	return "Hello Upload";
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello Upload");
+	response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -7,11 +7,7 @@ function start(route, handle) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 
-		var content = route(handle, pathname);
-
-		response.writeHead(200, {"Content-Type" : "text/plain"});
-		response.write(content);
-		response.end();
+		route(handle, pathname, response);
 	}).listen(8888);
 
 	console.log("Server has started.");
